b9de3581fdf9fb394d229534c84ee08cb360ce84
MUKTA ROY <muktaroy520@gmail.com>
2026-06-27 16:00:19 +0600

    useWishlist()

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,13 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
 import { CartProvider } from './context/CartContext'
+import { WishlistProvider } from './context/WishlistContext'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <CartProvider>
+        <WishlistProvider/>
         <App />
       </CartProvider>
     </BrowserRouter>
